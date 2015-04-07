b07df5a87444e9c945cd3e8947423162be1d7cae
jarnovanwijgerden <jarno.ajaxthebest@hotmail.com>
2015-04-07 07:05:17 -0700

    Layout gemaakt

---
+++ EindopdrachtIOS/EindopdrachtIOS/RestaurantsController.swift
@@ -0,0 +1,35 @@
+//
+//  RestaurantsController.swift
+//  EindopdrachtIOS
+//
+//  Created by User on 07/04/15.
+//  Copyright (c) 2015 Jarno. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantsController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
