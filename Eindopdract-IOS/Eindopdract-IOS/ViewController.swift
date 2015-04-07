d3267f8c561e359173f89c9ac03e6b2fc37036b6
louishol <louis_hol@hotmail.com>
2015-04-07 12:56:36 +0200

    Initial commit

---
+++ Eindopdract-IOS/Eindopdract-IOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Eindopdract-IOS
+//
+//  Created by Jarno van Wijgerden on 17/03/15.
+//  Copyright (c) 2015 Jarno van Wijgerden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
